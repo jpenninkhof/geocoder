8d84fdd86710db30f5aa240670f8132a4aba6788
Jan Penninkhof <jan@penninkhof.com>
2016-03-07 15:02:21 +0100

    Initial version

---
+++ geocoder.js
@@ -0,0 +1,21 @@
+var geocoder = require('node-geocoder') ("google", "https", {
+	apiKey: '<YOUR-KEY>'
+});
+var csv = require('csv');
+
+process.stdin
+	.pipe(csv.parse())
+	.pipe(csv.transform(function(data, callback){
+		setImmediate(function(){
+			geocoder.geocode(data[1])
+		    .then(function(res) {
+				data.push(res[0].latitude);
+				data.push(res[0].longitude);
+				callback(null, data);
+		    })
+		    .catch(function(err) {
+		        console.log(data[0] + " " + err);
+		    });
+		});}, {parallel: 20}))
+	.pipe(csv.stringify({quotedString: true}))
+	.pipe(process.stdout);
